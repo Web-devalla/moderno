342f8407b1a78fadbe31b7eef1a358ed243294f8
Your NameWeb-devalla <kolesnikalla91@ukr.net>
2020-09-13 18:14:24 +0300

    added new items page

--- app/js/main.js
+++ app/js/main.js
@@ -15,7 +15,29 @@ $('.product-slider__inner').slick({
     slidesToScroll: 4,
 });
 
-var mixer = mixitup('.products__inner-box');
 
+
+$(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: '$',
+});
+
+$('.icon-th-list').on('click', function(){
+    $('.product__item1').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon-th').removeClass('active');
+});
+
+$('.icon-th').on('click', function(){
+    $('.product__item1').removeClass('list');
+    $('.icon-th').addClass('active');
+    $('.icon-th-list').removeClass('active');
+});
+
+var mixer = mixitup('.products__inner-box');
 
 });
